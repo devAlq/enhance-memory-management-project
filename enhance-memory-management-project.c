43bbb3306e6f6f463c72aa4204b88147e473842b
KhalidDev <148249284+devAlq@users.noreply.github.com>
2026-02-02 14:18:42 +0300

    Create enhance-memory-management-project.c

---
+++ enhance-memory-management-project.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct Example {
+    double b;    // 8 bytes
+    int d;       // 4 bytes
+
+    char c;      // 1 byte
+    char a;      // 1 byte
+
+};
+
+int main() {
+    struct Example example;
+
+    printf("Size of struct Example: %lu bytes\n", sizeof(example));
+    printf("Address of a: %p\n", &example.a);
+    printf("Address of b: %p\n", &example.b);
+    printf("Address of c: %p\n", &example.c);
+    printf("Address of d: %p\n", &example.d);
+
+    return 0;
+}
